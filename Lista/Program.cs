35a09b492a4c08d3a08e54186f0b8d62c245efa6
Pedro Joaquim <pedro.joaquim@estudante.ifro.edu.br>
2023-07-27 11:46:24 -0400

    first commit

---
+++ Lista/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Lista
+{
+    static void Main(string[] args)
+    {
+        List<string> lista1 = new List<string>();
+        lista1.Add("a"); //add coisa lista
+        lista1.Add("b");
+        lista1.Add("c");
+
+        Console.WriteLine(lista1[2]); //imprime
+
+        Console.WriteLine(lista1.Count); //conta tamanho lista
+
+        foreach(string s in lista1) //percorre lsita
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
